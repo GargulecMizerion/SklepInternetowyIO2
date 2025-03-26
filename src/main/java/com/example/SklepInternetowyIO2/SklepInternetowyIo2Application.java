093753b097c72d2a6f5b86a8022ef0e358031dd9
GargulecMizerion <xmaszekx@gmail.com>
2025-03-26 19:33:48 +0100

    Project Init

---
+++ src/main/java/com/example/SklepInternetowyIO2/SklepInternetowyIo2Application.java
@@ -0,0 +1,13 @@
+package com.example.SklepInternetowyIO2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SklepInternetowyIo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SklepInternetowyIo2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SklepInternetowyIO2/SklepInternetowyIo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SklepInternetowyIO2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SklepInternetowyIo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
